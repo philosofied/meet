619f6569ce21b74d620d30ebb010002df784b517
Phil <phil.flrs@gmail.com>
2021-08-11 20:51:25 -0600

    working 4.4

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -19,7 +19,7 @@ const credentials = {
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
     auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    redirect_uris: ["https://philosofied.github.io/meet/"],
+    redirect_uris: ["https://philosofied.github.io/meet/", "http://localhost:3000/meet"],
     javascript_origins: ["https://philosofied.github.io", "http://localhost:3000"],
 };
 
@@ -27,7 +27,7 @@ const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
 const oAuth2Client = new google.auth.OAuth2(
     client_id,
     client_secret,
-    redirect_uris[0]
+    redirect_uris[1]
 );
 
 /**
