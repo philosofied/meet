a842fa93cd0a3fe02205bd2b638d300cacb3ac58
Phil <phil.flrs@gmail.com>
2021-08-14 12:27:39 -0600

    4.6 update index.js for atataus monitoring

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import * as atatus from 'atatus-spa';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    App / >
+    <
+    /React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
@@ -18,6 +20,6 @@ ReactDOM.render(
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
-atatus.config('37c2462244c9403c9d1d12c4f2de257b').install();
-// atatus.notify(new Error('Test Atatus Setup'));
+atatus.config('4c85786ab1164b31b5b4d484eaf98738').install();
+//</React.StrictMode>atatus.notify(new Error('Test Atatus Setup'));
 serviceWorkerRegistration.register();
